bc7523e80e048707f0867b76d8cea18a139723a3
deannapi <deannapi.mart@gmail.com>
2020-06-15 14:18:13 -0500

    added html homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,10 @@
 // this index.js collects the PACKAGED API routes from the routes/API/index.js
 const router = require('express').Router();
-
+const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
