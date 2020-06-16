6e539d56756a23143acd40be8d45ca0f488d001b
deannapi <deannapi.mart@gmail.com>
2020-06-16 10:49:13 -0500

    added function if user does not sign in

--- server.js
+++ server.js
@@ -2,24 +2,26 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+
 const session = require('express-session');
-
-const SequeilizeStore = require('connect-session-sequelize')(session.Store);
-
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequeilizeStore({
-    db: sequelize
-  })
-};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'alphaDog',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
